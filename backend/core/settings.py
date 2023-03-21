b1771fc75d56aa502e8b115e3dc9bf889b5ce7c2
Vadim <71ooo@mail.ru>
2023-03-21 17:30:42 +0300

    djangorestframework-simplejwt was created

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'drf_yasg',
+    'rest_framework_simplejwt',
 
     'app',
 ]
@@ -125,3 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
