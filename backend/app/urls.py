fffe8be35ddc68f7daa03deee9067b5cebd0c553
Vadim <71ooo@mail.ru>
2023-03-22 09:57:08 +0300

    redid pipenv to poetry

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import OrganizationList, ShopUpdate, export_shops_to_csv
+from .views import OrganizationAPIView, ShopUpdate, export_shops_to_csv
 
 urlpatterns = [
-    path('api/organizations/', OrganizationList.as_view()),
+    path('api/organizations/', OrganizationAPIView.as_view()),
     path('api/shops/<int:pk>/', ShopUpdate.as_view()),
     path('api/organizations/<int:id>/shops_file/', export_shops_to_csv),
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,33 +1,65 @@
-from rest_framework.generics import UpdateAPIView, ListAPIView
+import csv
+
+from django.core.mail import send_mail
+from django.http import HttpResponse
+from rest_framework.generics import ListAPIView, UpdateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Organization, Shop
 from .serializers import OrganizationSerializer, ShopSerializer
 
+# class OrganizationList(ListAPIView):
+#     queryset = Organization.objects.all()
+#     serializer_class = OrganizationSerializer
 
-class OrganizationList(ListAPIView):
-    queryset = Organization.objects.all()
-    serializer_class = OrganizationSerializer
+
+class OrganizationAPIView(APIView):
+
+    def get(self, request):
+        organizations = Organization.objects.all()
+        serializer = OrganizationSerializer(organizations, many=True)
+        return Response({"organizations": serializer.data})
 
 
 class ShopUpdate(UpdateAPIView):
     queryset = Shop.objects.all()
     serializer_class = ShopSerializer
+    lookup_field = 'pk'
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Shop updated successfully"})
+        else:
+            return Response({"message": "failed", "details": serializer.errors})
+
+    # def put(self):
+    #     """Отправка email"""
+    #     send_mail(
+    #         'Subject here',
+    #         'Here is the message.',
+    #         'from@example.com',
+    #         ['to@example.com'],
+    #         fail_silently=False)
+    #     return HttpResponse('Message was send')
 
 
 def export_shops_to_csv(request, id: int):
     """Экспорт Магазинов в csv
 
     Args:
-        id (int): id организации
+        id организации
 
     Returns:
         csv файл
     """
-    from django.http import HttpResponse
-    import csv
+    
     shops = Shop.objects.filter(organization_id=id)
     response = HttpResponse('text/csv')
-    response['Content-Disposition'] = 'attachment; filename=students.csv'
+    response['Content-Disposition'] = 'attachment; filename=shops.csv'
     writer = csv.writer(response)
     writer.writerow(
         ['id', 'name', 'description', 'address', 'index', 'is_deleted'])
